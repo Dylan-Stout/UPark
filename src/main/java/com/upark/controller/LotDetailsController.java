35188693d8484eb884ea4099e7d1e38039d3d2d0
Dylan Stout <dylan.d.stout@gmail.com>
2018-09-08 23:10:09 -0600

    Lot details page and controller

--- src/main/java/com/upark/controller/LotDetailsController.java
+++ src/main/java/com/upark/controller/LotDetailsController.java
@@ -1,5 +1,17 @@
 package com.upark.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class LotDetailsController {
+	
+	@GetMapping("/lotDetails")
+    public String greeting( Model model) {
+       
+        return "lot-details";
+    }
 
 }
