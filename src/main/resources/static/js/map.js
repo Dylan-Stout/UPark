86b35aebd8813914b56fbf573837fefcddecfde6
Dylan Stout <dylan.d.stout@gmail.com>
2018-09-08 23:47:30 -0600

    darknet detector source

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
    alert("Test"); 
 });
+
+var locations = [
+	  ['Union Parking Lot', -33.890542, 151.274856, 4],
+	  ['Presidents Circle Lot', -33.923036, 151.259052, 5],
+	  ['Warnock Engineering Lot', -34.028249, 151.157507, 3],
+	  ['Huntsman Lot', -33.80010128657071, 151.28747820854187, 2],
+	  ['Central Campus Lot', -33.950198, 151.259302, 1]
+];
